405d0a12f727e3041ed2333cfe4cebd7a1b21566
JENNIE <160710985+hi-jennie@users.noreply.github.com>
2024-07-09 20:22:52 +0800

    create Spiky and Stewie

--- src/main/java/intro/Test.java
+++ src/main/java/intro/Test.java
@@ -1,10 +1,8 @@
 package intro;
 
-/**
- * @author rd_qinglin_mu
- * @description test
- * @单据标识
- * @date 2024/7/9 15:53
- **/
+
 public class Test {
+    public static void main(String[] args) {
+        
+    }
 }
--- src/main/java/intro/exercise/Stewie.java
+++ src/main/java/intro/exercise/Stewie.java
@@ -1,11 +1,5 @@
 package intro.exercise;
 
-/**
- * @author rd_qinglin_mu
- * @description Stewie
- * @单据标识
- * @date 2024/7/9 16:33
- **/
 public class Stewie {
     public static void main(String[] args) {
         System.out.println("//////////////////////");
