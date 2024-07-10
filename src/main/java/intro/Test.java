066b8822559ac3e68e02bfb1f281852258acd7b2
hi-jennie <hi-jennie@outlook.com>
2024-07-10 12:38:29 +0800

    lab01: Completed Arithmetic.java

--- src/main/java/intro/Test.java
+++
@@ -1,8 +0,0 @@
-package intro;
-
-
-public class Test {
-    public static void main(String[] args) {
-        
-    }
-}
--- src/main/java/intro/exercise/Stewie.java
+++
@@ -1,9 +0,0 @@
-package intro.exercise;
-
-public class Stewie {
-    public static void main(String[] args) {
-        System.out.println("//////////////////////");
-        System.out.println("|| Victory is mine! ||");
-        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
-    }
-}
