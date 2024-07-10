22691502ea17440d7c8f733fdb4d3678181693aa
hi-jennie <hi-jennie@outlook.com>
2024-07-10 21:05:49 +0800

    List exercises

--- skeleton-sp24/hw0b/src/ListExercises.java
+++ skeleton-sp24/hw0b/src/ListExercises.java
@@ -1,29 +1,60 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
 
-    /** Returns the total sum in a list of integers */
-	public static int sum(List<Integer> L) {
-        // TODO: Fill in this function.
-        return 0;
+    /**
+     * Returns the total sum in a list of integers
+     */
+    public static int sum(List<Integer> L) {
+        int sum = 0;
+        for (Integer i : L) {
+            sum = sum + i;
+        }
+        return sum;
     }
 
-    /** Returns a list containing the even numbers of the given list */
+    /**
+     * Returns a list containing the even numbers of the given list
+     */
     public static List<Integer> evens(List<Integer> L) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> evenNum = new ArrayList<>();
+        for (Integer i : L) {
+            if (i % 2 == 0) {
+                evenNum.add(i);
+            }
+        }
+        return evenNum;
     }
 
-    /** Returns a list containing the common item of the two given lists */
+    /**
+     * Returns a list containing the common item of the two given lists
+     */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> common = new ArrayList<>();
+        for (Integer i : L1) {
+            for (Integer integer : L2) {
+                if (i == integer && !common.contains(integer)) {
+                    common.add(integer);
+                }
+            }
+        }
+        return common;
     }
 
 
-    /** Returns the number of occurrences of the given character in a list of strings. */
+    /**
+     * Returns the number of occurrences of the given character in a list of strings.
+     */
     public static int countOccurrencesOfC(List<String> words, char c) {
-        // TODO: Fill in this function.
-        return 0;
+        int cnt = 0;
+        for (String word : words) {
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) == c) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
     }
 }
